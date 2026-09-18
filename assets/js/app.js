e13ae68ed6392fb5f876f7c7c37573d8a7130dc2
ospbialogrady <osp.bialogrady@gmail.com>
2026-09-18 08:11:43 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -136,7 +136,7 @@ heroTimeline
 
 const revealElements =
 document.querySelectorAll(
-".mission-card, .why-item, .operation-box, .intro-card, .number-card, .gallery-item, .home-project, .home-equipment-image, .home-equipment-copy, .home-origin-copy, .home-origin-visual"
+".mission-card, .why-item, .operation-box, .intro-card, .number-card, .gallery-item, .home-project, .home-equipment-image, .home-equipment-copy, .home-origin-copy, .home-origin-visual, .join-card, .campaign-card, .reactivation-copy, .reactivation-visual"
 );
 
 
